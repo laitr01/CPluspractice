9428ce9402a7ea8faadeaddede87ed99e8eb8f80
ltrach <ltrach@yahoo-corp.jp>
2018-08-20 16:06:35 +0700

    init()

---
+++ app/src/androidTest/java/com/bestapplication/cpluspractice/ExampleInstrumentedTest.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2018 Yahoo! JAPAN Corporation. All rights reserved.
+ */
+
+package com.bestapplication.cpluspractice
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.bestapplication.cpluspractice", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/bestapplication/cpluspractice/MainActivity.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 Yahoo! JAPAN Corporation. All rights reserved.
+ */
+
+package com.bestapplication.cpluspractice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
